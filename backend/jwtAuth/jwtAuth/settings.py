172f8e0427cd3366e07a16eb6be8eddf00a7cb70
Blessing O. ADEKUNLE <123912786+obdif@users.noreply.github.com>
2024-07-04 22:10:45 +0100

    docker commit

--- backend/jwtAuth/jwtAuth/settings.py
+++ backend/jwtAuth/jwtAuth/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
         'NAME': 'authdb',
         'USER': 'root',
         'PASSWORD': 'Devops@2024',
-        'HOST': 'localhost',
+        'HOST': 'mysql  ',
         'PORT': '3306',
     }
 }
